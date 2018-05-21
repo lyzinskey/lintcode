823c07272e0ff579b16b635272e0fbd5f37d9169
Inskey <36866529+lyzinskey@users.noreply.github.com>
2018-05-21 11:14:22 -0700

    Update code.java

--- 428. Pow-x- n/code.java
+++ 428. Pow-x- n/code.java
@@ -6,7 +6,6 @@
 //Pow(1, 0) = 1
 
 
-    //recursion solution
     public double myPow(double x, int n) {
         double ans = Integer.MIN_VALUE;
         if (n > 0){
@@ -19,6 +18,7 @@
         return ans;
     }
 
+    //recursion solution
     private static double fastPow(double x, int n){
         if (x == 0) {
             return 0;
@@ -30,13 +30,36 @@
         if (n % 2 == 0) {
             double temp = fastPow(x, n / 2);
             return temp * temp;
-        } 
+        }
         //n is odd, have to multiple x one more time
         else {
             double temp = fastPow(x, n / 2);
             return temp * temp * x;
         }
     }
+
+
+//    //non-recursion solution
+//    private static double fastPow(double x, int n){
+//        if (x == 0) {
+//            return 0;
+//        }
+//        if (n == 0) {
+//            return 1;
+//        }
+//
+//        double ans = 1;
+//        double base = x;
+//        while (n != 0){
+//            //n is odd, have to multiple base one more time
+//            if (n % 2 == 1 || n % 2 == -1) {
+//                ans *= base;
+//            }
+//            base *= base;
+//            n = n / 2;
+//        }
+//        return ans;
+//    }
 
 
 

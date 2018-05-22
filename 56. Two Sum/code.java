356354df9e6c76faba73f5ed3aae492c8d4f80a6
Inskey <36866529+lyzinskey@users.noreply.github.com>
2018-05-21 20:46:08 -0700

    Update code.java

--- 56. Two Sum/code.java
+++ 56. Two Sum/code.java
@@ -49,27 +49,29 @@
 //             return o1.getValue().compareTo(o2.getValue());
 //         }
 //     }
-//     public int[] twoSum(int[] numbers, int target) {
-//         // write your code here
 
+//     public int[] twoSum(int[] numbers, int target) {
 //         Pair[] number = new Pair[numbers.length];
-//         for(int i=0;i<numbers.length;i++) {
+//         for (int i = 0; i < numbers.length; i++) {
 //             number[i] = new Pair(numbers[i], i);
 //         }
 //         Arrays.sort(number, new ValueComparator());
-//         int L=0, R =  numbers.length-1;
-//         while(L<R) {
-//             if( number[L].getValue() + number[R].getValue() == target) {
+//         int L = 0, R = numbers.length-1;
+//         while (L < R) {
+//             if (number[L].getValue() + number[R].getValue() == target){
 //                 int t1 = number[L].index;
 //                 int t2 = number[R].index;
 //                 int[] result = {Math.min(t1,t2), Math.max(t1,t2)};
 //                 return result;
 //             }
-//             if( number[L].getValue() + number[R].getValue() < target) {
+//             if (number[L].getValue() + number[R].getValue() < target){
 //                 L++;
-//             } else {
+//             } 
+//             else {
 //                 R--;
 //             }
 //         }
 //         return null;
 //     }
+
+

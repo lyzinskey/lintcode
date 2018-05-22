352419c5c132df1fe116563543b80843aac4097c
Inskey <36866529+lyzinskey@users.noreply.github.com>
2018-05-21 18:06:09 -0700

    Update code.java

--- 56. Two Sum/code.java
+++ 56. Two Sum/code.java
@@ -12,14 +12,15 @@
 
     // using HashMap
     // O(n) Space, O(n) Time
-    public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            int complement = target - nums[i];
-            if (map.containsKey(complement)) {
-                return new int[]{map.get(complement), i};
+    public int[] twoSum(int[] numbers, int target) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (map.get(numbers[i]) != null) {
+                int[] result = {map.get(numbers[i]), i};
+                return result;
             }
-            map.put(nums[i], i);
+            map.put(target - numbers[i], i);
         }
         return null;
     }

4963ad4cba18a32420d25ebd4b0317660fb6e282
Inskey <36866529+lyzinskey@users.noreply.github.com>
2018-05-23 13:35:32 -0700

    Update code.java

--- 464. Sort Integers II/code.java
+++ 464. Sort Integers II/code.java
@@ -58,4 +58,57 @@
     
     
     
-    
+
+    //merge sort
+    public void sortIntegers2(int[] A) {
+        if (A == null || A.length == 0){
+            return;
+        }
+        int[] temp = new int[A.length];
+        mergeSort(A, 0, A.length - 1, temp);
+    }
+
+    private void mergeSort(int[] A, int start, int end, int[] temp){
+        if (start >= end){
+            return;
+        }
+
+        mergeSort(A, start, (start + end) / 2, temp);
+        mergeSort(A, (start + end) / 2 + 1, end, temp);
+        merge(A, start, end, temp);
+    }
+
+    private void merge(int[] A, int start, int end, int[] temp){
+        int left = start; 
+        int middle = (start + end) / 2;
+        int right = middle + 1;
+        int index = left;
+
+        while (left <= middle && right <= end){
+            if (A[left] < A[right]){
+                temp[index] = A[left];
+                left++;
+            }
+            else {
+                temp[index] = A[right];
+                right++;
+            }
+            index++;
+        }
+
+        while (left <= middle){
+            temp[index] = A[left];
+            index++;
+            left++;
+        }
+
+        while (right <= end){
+            temp[index] = A[right];
+            index++;
+            right++;
+        }
+        
+        for (int i = start; i <= end; i++) {
+            A[i] = temp[i];
+        }        
+    }    

30bffe40720628aedd3618b7e591b9299fc6e3f5
Inskey <36866529+lyzinskey@users.noreply.github.com>
2018-06-17 19:50:31 -0700

    Create code.java

---
+++ 154. Regular Expression Matching/code.java
@@ -0,0 +1,88 @@
+//Implement regular expression matching with support for '.' and '*'.
+//
+//'.' Matches any single character.
+//'*' Matches zero or more of the preceding element.
+//The matching should cover the entire input string (not partial).
+//        
+//The function prototype should be:
+//
+//bool isMatch(string s, string p)
+//        
+//Example
+//isMatch("aa","a") → false
+//isMatch("aa","aa") → true
+//isMatch("aaa","aa") → false
+//isMatch("aa", "a*") → true
+//isMatch("aa", ".*") → true
+//isMatch("ab", ".*") → true
+//isMatch("aab", "c*a*b") → true
+
+
+
+
+public class Solution {
+    /**
+     * @param s: A string 
+     * @param p: A string includes "." and "*"
+     * @return: A boolean
+     */
+    public boolean isMatch(String s, String p) {
+        if (s == null || p == null) {
+            return false;
+        }
+        
+        boolean[][] memo = new boolean[s.length()][p.length()];
+        boolean[][] visited = new boolean[s.length()][p.length()];
+        
+        return isMatchHelper(s, 0, p, 0, memo, visited);
+    }
+    
+    private boolean isMatchHelper(String s, int sIndex,
+                                  String p, int pIndex,
+                                  boolean[][] memo,
+                                  boolean[][] visited) {
+        // "" == ""
+        if (pIndex == p.length()) {
+            return sIndex == s.length();
+        }
+        
+        if (sIndex == s.length()) {
+            return isEmpty(p, pIndex);
+        }
+        
+        if (visited[sIndex][pIndex]) {
+            return memo[sIndex][pIndex];
+        }
+        
+        char sChar = s.charAt(sIndex);
+        char pChar = p.charAt(pIndex);
+        boolean match;
+        
+        // consider a* as a bundle
+        if (pIndex + 1 < p.length() && p.charAt(pIndex + 1) == '*') {
+            match = isMatchHelper(s, sIndex, p, pIndex + 2, memo, visited) ||
+                charMatch(sChar, pChar) && isMatchHelper(s, sIndex + 1, p, pIndex, memo, visited);
+        } else {
+            match = charMatch(sChar, pChar) && 
+                isMatchHelper(s, sIndex + 1, p, pIndex + 1, memo, visited);
+        }
+        
+        visited[sIndex][pIndex] = true;
+        memo[sIndex][pIndex] = match;
+        return match;
+    }
+    
+    private boolean charMatch(char sChar, char pChar) {
+        return sChar == pChar || pChar == '.';
+    }
+    
+    private boolean isEmpty(String p, int pIndex) {
+        for (int i = pIndex; i < p.length(); i += 2) {
+            if (i + 1 >= p.length() || p.charAt(i + 1) != '*') {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+

2dacfc33cb95de4be36af5d328ac9530cf1188ba
Inskey <36866529+lyzinskey@users.noreply.github.com>
2018-06-17 19:30:50 -0700

    Create code.java

---
+++ 192. Wildcard Matching/code.java
@@ -0,0 +1,86 @@
+//Implement wildcard pattern matching with support for '?' and '*'.
+
+//'?' Matches any single character.
+//'*' Matches any sequence of characters (including the empty sequence).
+//The matching should cover the entire input string (not partial).
+
+//Example
+//isMatch("aa","a") → false
+//isMatch("aa","aa") → true
+//isMatch("aaa","aa") → false
+//isMatch("aa", "*") → true
+//isMatch("aa", "a*") → true
+//isMatch("ab", "?*") → true
+//isMatch("aab", "c*a*b") → false
+
+
+
+public class Solution {
+    /**
+     * @param s: A string 
+     * @param p: A string includes "?" and "*"
+     * @return: is Match?
+     */
+     
+    //使用深度优先搜索 + 记忆化的版本。
+    //用一个二维的 boolean 数组来当记忆化数组，记录 s 从 sIndex 开始的后缀能否匹配上 p 从 pIndex 开始的后缀
+    //通过记忆化，把原本2^n的时间复杂度优化到了n^2
+    public boolean isMatch(String s, String p) {
+        if (s == null || p == null) {
+            return false;
+        }
+        
+        boolean[][] memo = new boolean[s.length()][p.length()];
+        boolean[][] visited = new boolean[s.length()][p.length()];
+        return isMatchHelper(s, 0, p, 0, memo, visited);
+    }
+    
+    private boolean isMatchHelper(String s, int sIndex,
+                                  String p, int pIndex,
+                                  boolean[][] memo,
+                                  boolean[][] visited) {
+        // 如果 p 从pIdex开始是空字符串了，那么 s 也必须从 sIndex 是空才能匹配上
+        if (pIndex == p.length()) {
+            return sIndex == s.length();
+        }
+        
+        // 如果 s 从 sIndex 是空，那么p 必须全是 * 
+        if (sIndex == s.length()) {
+            return allStar(p, pIndex);
+        }
+        
+        if (visited[sIndex][pIndex]) {
+            return memo[sIndex][pIndex];
+        }
+        
+        char sChar = s.charAt(sIndex);
+        char pChar = p.charAt(pIndex);
+        boolean match;
+        
+        if (pChar == '*') {
+            match = isMatchHelper(s, sIndex, p, pIndex + 1, memo, visited) ||
+                isMatchHelper(s, sIndex + 1, p, pIndex, memo, visited);
+        } else {
+            match = charMatch(sChar, pChar) &&
+                isMatchHelper(s, sIndex + 1, p, pIndex + 1, memo, visited);
+        }
+        
+        visited[sIndex][pIndex] = true;
+        memo[sIndex][pIndex] = match;
+        return match;
+    }
+    
+    private boolean charMatch(char sChar, char pChar) {
+        return (sChar == pChar || pChar == '?');
+    }
+    
+    private boolean allStar(String p, int pIndex) {
+        for (int i = pIndex; i < p.length(); i++) {
+            if (p.charAt(i) != '*') {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+

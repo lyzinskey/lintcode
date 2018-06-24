4090fd685edc475ceb318fbd82fedacf31a5cba0
Inskey <36866529+lyzinskey@users.noreply.github.com>
2018-06-23 18:38:14 -0700

    Create code.java

---
+++ 167. Add Two Numbers/code.java
@@ -0,0 +1,52 @@
+//You have two numbers represented by a linked list, where each node contains a single digit. 
+//The digits are stored in reverse order, such that the 1's digit is at the head of the list. 
+//Write a function that adds the two numbers and returns the sum as a linked list.
+
+//Example
+//Given 7->1->6 + 5->9->2. That is, 617 + 295.
+//
+//Return 2->1->9. That is 912.
+//
+//Given 3->1->5 and 5->9->2, return 8->0->8.
+
+
+
+/**
+ * Definition for ListNode
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+    /**
+     * @param l1: the first list
+     * @param l2: the second list
+     * @return: the sum list of l1 and l2 
+     */
+    public ListNode addLists(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode p = l1, q = l2, curr = dummyHead;
+        int carry = 0;
+        while (p != null || q != null) {
+            int x = (p != null) ? p.val : 0;
+            int y = (q != null) ? q.val : 0;
+            int sum = carry + x + y;
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+            if (p != null) p = p.next;
+            if (q != null) q = q.next;
+        }
+        if (carry > 0) {
+            curr.next = new ListNode(carry);
+        }
+        return dummyHead.next;
+    }
+}
+

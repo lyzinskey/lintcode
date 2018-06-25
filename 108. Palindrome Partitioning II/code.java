8cfd8dbab0d1fffdab3d669e57bf57e5471ee712
Inskey <36866529+lyzinskey@users.noreply.github.com>
2018-06-25 15:20:20 -0700

    Create code.java

---
+++ 108. Palindrome Partitioning II/code.java
@@ -0,0 +1,63 @@
+//Given a string s, cut s into some substrings such that every substring is a palindrome.
+
+//Return the minimum cuts needed for a palindrome partitioning of s.
+
+//Example
+//Given s = "aab",
+//
+//Return 1 since the palindrome partitioning ["aa", "b"] could be produced using 1 cut.
+
+
+
+public class Solution {
+    /**
+     * @param s: A string
+     * @return: An integer
+     */
+    public int minCut(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+
+        // preparation
+        boolean[][] isPalindrome = getIsPalindrome(s);
+        
+        // initialize
+        int[] f = new int[s.length() + 1];
+        f[0] = 0;
+        
+        // main
+        for (int i = 1; i <= s.length(); i++) {
+            f[i] = Integer.MAX_VALUE; // or f[i] = i
+            for (int j = 0; j < i; j++) {
+                if (isPalindrome[j][i - 1]) {
+                    f[i] = Math.min(f[i], f[j] + 1);
+                }
+            }
+        }
+
+        return f[s.length()] - 1;
+    }
+    
+    private boolean[][] getIsPalindrome(String s) {
+        boolean[][] isPalindrome = new boolean[s.length()][s.length()];
+
+        for (int i = 0; i < s.length(); i++) {
+            isPalindrome[i][i] = true;
+        }
+        for (int i = 0; i < s.length() - 1; i++) {
+            isPalindrome[i][i + 1] = (s.charAt(i) == s.charAt(i + 1));
+        }
+
+        for (int length = 2; length < s.length(); length++) {
+            for (int start = 0; start + length < s.length(); start++) {
+                isPalindrome[start][start + length]
+                    = isPalindrome[start + 1][start + length - 1] && s.charAt(start) == s.charAt(start + length);
+            }
+        }
+
+        return isPalindrome;
+    }
+    
+}
+

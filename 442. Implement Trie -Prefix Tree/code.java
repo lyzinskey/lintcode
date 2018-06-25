8720c29b64b8fb315d5c90d8b7abf28e2e663dd9
Inskey <36866529+lyzinskey@users.noreply.github.com>
2018-06-25 16:14:21 -0700

    Create code.java

---
+++ 442. Implement Trie -Prefix Tree/code.java
@@ -0,0 +1,112 @@
+//Implement a trie with insert, search, and startsWith methods.
+
+//You may assume that all inputs are consist of lowercase letters a-z.
+
+//Example
+//insert("lintcode")
+//search("code")
+//>>> false
+//startsWith("lint")
+//>>> true
+//startsWith("linterror")
+//>>> false
+//insert("linterror")
+//search("lintcode)
+//>>> true
+//startsWith("linterror")
+//>>> true
+
+
+public class Trie {
+    
+    private TrieNode root;
+    
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    /*
+     * @param word: a word
+     * @return: nothing
+     */
+    public void insert(String word) {
+        TrieNode node = root;
+        for (int i = 0; i < word.length(); i++) {
+            char currentChar = word.charAt(i);
+            if (!node.containsKey(currentChar)) {
+                node.put(currentChar, new TrieNode());
+            }
+            node = node.get(currentChar);
+        }
+        node.setEnd();
+    }
+
+    /*
+     * @param word: A string
+     * @return: if the word is in the trie.
+     */
+    public boolean search(String word) {
+       TrieNode node = searchPrefix(word);
+       return node != null && node.isEnd();
+    }
+    
+    
+    // search a prefix or whole key in trie and
+    // returns the node where search ends
+    private TrieNode searchPrefix(String word) {
+        TrieNode node = root;
+        for (int i = 0; i < word.length(); i++) {
+           char curLetter = word.charAt(i);
+           if (node.containsKey(curLetter)) {
+               node = node.get(curLetter);
+           } else {
+               return null;
+           }
+        }
+        return node;
+    }
+    
+
+    /*
+     * @param prefix: A string
+     * @return: if there is any word in the trie that starts with the given prefix.
+     */
+    public boolean startsWith(String prefix) {
+        TrieNode node = searchPrefix(prefix);
+        return node != null;
+    }
+    
+}
+
+
+class TrieNode {
+
+    // R links to node children
+    private TrieNode[] links;
+
+    private final int R = 26;
+
+    private boolean isEnd;
+
+    public TrieNode() {
+        links = new TrieNode[R];
+    }
+
+    public boolean containsKey(char ch) {
+        return links[ch -'a'] != null;
+    }
+    public TrieNode get(char ch) {
+        return links[ch -'a'];
+    }
+    public void put(char ch, TrieNode node) {
+        links[ch -'a'] = node;
+    }
+    public void setEnd() {
+        isEnd = true;
+    }
+    public boolean isEnd() {
+        return isEnd;
+    }
+}
+
+

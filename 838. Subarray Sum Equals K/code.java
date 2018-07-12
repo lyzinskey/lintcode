b90104fc4f8e2233cb0a1da238029bc47b7f5a56
Inskey <36866529+lyzinskey@users.noreply.github.com>
2018-07-11 21:53:50 -0700

    Update code.java

--- 838. Subarray Sum Equals K/code.java
+++ 838. Subarray Sum Equals K/code.java
@@ -5,6 +5,27 @@
 //Input:nums = [1,1,1], k = 2
 /Output: 2
 
+// The idea behind this approach is as follows: 
+// If the cumulative sum(represented by sum[i] for sum upto i^{th} index) upto two indices is the same, 
+// the sum of the elements lying in between those indices is zero. 
+// Extending the same thought further, if the cumulative sum upto two indices, 
+// say i and j is at a difference of k i.e. if sum[i] - sum[j] = k, 
+// the sum of elements lying between indices i and j is k.
+//
+// Based on these thoughts, we make use of a hashmap map 
+// which is used to store the cumulative sum upto all the indices possible 
+// along with the number of times the same sum occurs. 
+// We store the data in the form: (sum_i, number of occurences of sum_i). 
+// We traverse over the array nums and keep on finding the cumulative sum. 
+// Every time we encounter a new sum, we make a new entry in the hashmap corresponding to that sum. 
+// If the same sum occurs again, we increment the count corresponding to that sum in the hashmap. 
+// Further, for every sum encountered, 
+// we also determine the number of times the sum sum-k has occured already, 
+// since it will determine the number of times a subarray with sum k has occured upto the current index. 
+// We increment the countcount by the same amount.
+//
+// After the complete array has been traversed, the countcount gives the required result.
+
 
     public int subarraySumEqualsK(int[] nums, int k) {
         int result = 0;

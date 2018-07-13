5a4e18e6b2f9179877289890c2f75a07d56757f5
Inskey <36866529+lyzinskey@users.noreply.github.com>
2018-07-13 15:01:12 -0700

    Update code.java

--- 65. Median of two Sorted Arrays/code.java
+++ 65. Median of two Sorted Arrays/code.java
@@ -11,6 +11,8 @@
 
 
 // 基于 FindKth 的算法
+// 时间复杂度O(log(m + n))
+//
 public class Solution {
 
     public double findMedianSortedArrays(int[] A, int[] B) {
@@ -45,6 +47,84 @@ public class Solution {
         }
         else {
             return findKth(A, B, indexA, indexB + k / 2, k - k / 2);
+        }
+    }
+}
+
+
+
+
+// 基于二分的算法
+// 时间复杂度 O(log(range) * (log(n) + log(m)))
+// 其中 range 为最小和最大的整数之间的范围。
+//
+public class Solution {
+
+    public double findMedianSortedArrays(int[] A, int[] B) {
+        int length = A.length + B.length;
+        
+        if (length % 2 == 0) {
+            return (findKth(A, B, length / 2) + findKth(A, B, length / 2 + 1)) / 2.0;
+        }
+        else {
+            return findKth(A, B, length / 2 + 1);
+        }
+    }
+    
+    
+    private int findKth(int[] A, int[] B, int k) {
+        if (A.length == 0) {
+            return B[k - 1];
+        }
+        if (B.length == 0) {
+            return A[k - 1];
+        }
+        
+        int start = Math.min(A[0], B[0]);
+        int end = Math.max(A[A.length - 1], B[B.length - 1]);
+        
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            
+            if (countSmallerOrEqual(A, mid) + countSmallerOrEqual(B, mid) < k) {
+                start = mid;
+            }
+            else {
+                end = mid;
+            }
+        }
+        
+        if (countSmallerOrEqual(A, start) + countSmallerOrEqual(B, start) >= k) {
+            return start;
+        }
+        else {
+            return end;
+        }
+    }
+    
+    private int countSmallerOrEqual(int[] A, int target) {
+        int start = 0;
+        int end = A.length - 1;
+        
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            
+            if (A[mid] <= target) {
+                start = mid;
+            }
+            else {
+                end = mid;
+            }
+        }
+        
+        if (A[start] > target) {
+            return start;
+        }
+        else if (A[end] > target) {
+            return end;
+        }
+        else {
+            return A.length;
         }
     }
 }

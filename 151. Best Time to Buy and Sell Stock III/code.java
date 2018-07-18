884458955e138c2b2ea4726160776c337d7882ac
Inskey <36866529+lyzinskey@users.noreply.github.com>
2018-07-17 17:27:15 -0700

    Update code.java

--- 151. Best Time to Buy and Sell Stock III/code.java
+++ 151. Best Time to Buy and Sell Stock III/code.java
@@ -13,10 +13,10 @@
         int buy1 = Integer.MIN_VALUE, buy2 = Integer.MIN_VALUE;
         int sell1 = 0, sell2 = 0;
         for (int price : prices) {
-            buy1 = Math.max(buy1, -price);
-            sell1 = Math.max(sell1, buy1 + price);
-            buy2 = Math.max(buy2, sell1 - price);
-            sell2 = Math.max(sell2, buy2 + price);
+            buy1 = buy1 > -price ? buy1 : -price;
+            sell1 = sell1 > buy1 + price ? sell1 : buy1 + price;
+            buy2 = buy2 > sell1 - price ? buy2 : sell1 - price;
+            sell2 = sell2 > buy2 + price ? sell2 : buy2 + price;
         }
         return sell2;
     }

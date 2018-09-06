1687767f8c8b1a4a11c866e25578b6327cab32fc
Inskey <lyzinskey@gmail.com>
2018-09-05 18:52:12 -0700

    Update code.java

--- 108. Palindrome Partitioning II/code.java
+++ 108. Palindrome Partitioning II/code.java
@@ -14,50 +14,39 @@ public class Solution {
      * @param s: A string
      * @return: An integer
      */
-    public int minCut(String s) {
-        if (s == null || s.length() == 0) {
+    public int minCut(String input) {
+        if (input.length() <= 1) {
             return 0;
         }
-
-        // preparation
-        boolean[][] isPalindrome = getIsPalindrome(s);
-        
-        // initialize
-        int[] f = new int[s.length() + 1];
-        f[0] = 0;
-        
-        // main
-        for (int i = 1; i <= s.length(); i++) {
-            f[i] = Integer.MAX_VALUE; // or f[i] = i
-            for (int j = 0; j < i; j++) {
-                if (isPalindrome[j][i - 1]) {
-                    f[i] = Math.min(f[i], f[j] + 1);
+        char[] array = input.toCharArray();
+        int[] dp = new int[array.length];
+        for (int i = 0; i < dp.length; i++) {
+            if (isPalindrome(array, 0, i)) {
+                dp[i] = 0;
+            }
+            else {
+                dp[i] = i;
+                for (int j = 1; j <= i; j++) {
+                    if (isPalindrome(array, j, i)) {
+                        dp[i] = Math.min(dp[i], dp[j - 1] + 1);
+                    }
                 }
             }
         }
-
-        return f[s.length()] - 1;
+        return dp[dp.length - 1];
     }
-    
-    private boolean[][] getIsPalindrome(String s) {
-        boolean[][] isPalindrome = new boolean[s.length()][s.length()];
 
-        for (int i = 0; i < s.length(); i++) {
-            isPalindrome[i][i] = true;
-        }
-        for (int i = 0; i < s.length() - 1; i++) {
-            isPalindrome[i][i + 1] = (s.charAt(i) == s.charAt(i + 1));
-        }
-
-        for (int length = 2; length < s.length(); length++) {
-            for (int start = 0; start + length < s.length(); start++) {
-                isPalindrome[start][start + length]
-                    = isPalindrome[start + 1][start + length - 1] && s.charAt(start) == s.charAt(start + length);
+    private boolean isPalindrome(char[] input, int left, int right) {
+        while (left < right) {
+            if (input[left] != input[right]) {
+                return false;
             }
+            left++;
+            right--;
         }
-
-        return isPalindrome;
-    }
-    
+        return true;
+    }    
 }
+
+
 

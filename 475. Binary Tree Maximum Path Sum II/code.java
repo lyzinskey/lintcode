60a7cac5bdd870cb8b06e5659267d7d4459a4817
Inskey <lyzinskey@gmail.com>
2018-09-11 14:55:01 -0700

    Create code.java

---
+++ 475. Binary Tree Maximum Path Sum II/code.java
@@ -0,0 +1,40 @@
+//Given a binary tree, find the maximum path sum from root.
+
+//The path may end at any node in the tree and contain at least one node in it.
+
+//Example
+//Given the below binary tree:
+//
+//    1
+//   / \
+//  2   3
+//return 4. (1->3)
+
+
+
+
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+    public int maxPathSum2(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = maxPathSum2(root.left);
+        int right = maxPathSum2(root.right);
+        return root.val + Math.max(0, Math.max(left, right));
+    }
+}
+
+
+

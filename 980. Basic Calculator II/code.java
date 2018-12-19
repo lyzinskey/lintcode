689666cb7068b264c7dada390d28feb0dcbf5303
Inskey <lyzinskey@gmail.com>
2018-12-18 17:57:49 -0800

    Create code.java

---
+++ 980. Basic Calculator II/code.java
@@ -0,0 +1,60 @@
+//Implement a basic calculator to evaluate a simple expression string.
+
+//The expression string contains only non-negative integers, +, -, *, / operators and empty spaces . 
+//The integer division should truncate toward zero.
+
+//You may assume that the given expression is always valid.
+
+//Example
+//"3+2*2" = 7
+//" 3/2 " = 1
+//" 3+5 / 2 " = 5
+
+//Notice
+//Do not use the eval built-in library function.
+
+
+
+
+public class Solution {
+
+    public static int calculate(String s) {
+        if (s == null || s.length() == 0) return 0;
+
+        int len = s.length();
+        Stack<Integer> stack = new Stack<Integer>();
+        int num = 0;
+        char sign = '+';
+        for (int i = 0; i < len; i++) {
+            if (Character.isDigit(s.charAt(i))) {
+                num = num * 10 + s.charAt(i) - '0';
+            }
+            if ((!Character.isDigit(s.charAt(i)) && ' ' != s.charAt(i)) || i == len - 1) {
+
+                if (sign == '-') {
+                    stack.push(-num);
+                }
+                if (sign == '+') {
+                    stack.push(num);
+                }
+                if (sign == '*') {
+                    stack.push(stack.pop() * num);
+                }
+                if (sign == '/') {
+                    stack.push(stack.pop() / num);
+                }
+                sign = s.charAt(i);
+                num = 0;
+            }
+        }
+
+        int re = 0;
+        for (int i : stack) {
+            re += i;
+        }
+        return re;
+    }
+}
+
+
+

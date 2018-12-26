ae4dfe2dd9ebe641e2499ec62f335f5e2fb91a28
Inskey <lyzinskey@gmail.com>
2018-12-26 12:13:41 -0800

    Update code.java

--- 433. Number of Islands/code.java
+++ 433. Number of Islands/code.java
@@ -18,8 +18,8 @@
 //return 3.
 
 
-class Solution {
-    public int numIslands(char[][] grid) {
+public class Solution {
+    public int numIslands(boolean[][] grid) {
         if (grid.length == 0 || grid[0].length == 0) {
             return 0;
         }
@@ -27,8 +27,8 @@ class Solution {
         int result = 0;
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] == '1') {
-                    grid[i][j] = '0';
+                if (grid[i][j]) {
+                    grid[i][j] = false;
                     result++;
                     BFS(grid, i, j);
                 }
@@ -37,7 +37,7 @@ class Solution {
         return result;
     }
 
-    private void BFS(char[][] grid, int i, int j) {
+    private void BFS(boolean[][] grid, int i, int j) {
         Queue<int[]> queue = new LinkedList<>();
         queue.offer(new int[]{i, j});
 
@@ -46,27 +46,27 @@ class Solution {
             int x = array[0];
             int y = array[1];
 
-            if (!outOfBound(grid, x + 1, y) && grid[x + 1][y] == '1') {
-                grid[x + 1][y] = '0';
+            if (!outOfBound(grid, x + 1, y) && grid[x + 1][y]) {
+                grid[x + 1][y] = false;
                 queue.offer(new int[]{x + 1, y});
             }
-            if (!outOfBound(grid, x - 1, y) && grid[x - 1][y] == '1') {
-                grid[x - 1][y] = '0';
+            if (!outOfBound(grid, x - 1, y) && grid[x - 1][y]) {
+                grid[x - 1][y] = false;
                 queue.offer(new int[]{x - 1, y});
             }
-            if (!outOfBound(grid, x, y + 1) && grid[x][y + 1] == '1') {
-                grid[x][y + 1] = '0';
+            if (!outOfBound(grid, x, y + 1) && grid[x][y + 1]) {
+                grid[x][y + 1] = false;
                 queue.offer(new int[]{x, y + 1});
             }
-            if (!outOfBound(grid, x, y - 1) && grid[x][y - 1] == '1') {
-                grid[x][y - 1] = '0';
+            if (!outOfBound(grid, x, y - 1) && grid[x][y - 1]) {
+                grid[x][y - 1] = false;
                 queue.offer(new int[]{x, y - 1});
             }
 
         }
     }
 
-    private boolean outOfBound(char[][] grid, int x, int y) {
+    private boolean outOfBound(boolean[][] grid, int x, int y) {
         int row = grid.length;
         int col = grid[0].length;
         return x < 0 || x >= row || y < 0 || y >= col;
@@ -74,6 +74,47 @@ class Solution {
 }
 
 
+
+
+
+
+public class Solution {
+    public int numIslands(boolean[][] grid) {
+        if (grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
+
+        int result = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j]) {                    
+                    result++;
+                    DFS(grid, i, j);
+                }
+            }
+        }
+        return result;
+    }
+
+    private void DFS(boolean[][] grid, int i, int j) {
+        if (outOfBound(grid, i, j) || !grid[i][j]) {            
+            return;
+        }    
+        
+        grid[i][j] = false;                    
+        DFS(grid, i + 1, j);
+        DFS(grid, i - 1, j);
+        DFS(grid, i, j + 1);
+        DFS(grid, i, j - 1);
+    }
+
+    private boolean outOfBound(boolean[][] grid, int x, int y) {
+        int row = grid.length;
+        int col = grid[0].length;
+        return x < 0 || x >= row || y < 0 || y >= col;
+    }
+
+}
 
 
 

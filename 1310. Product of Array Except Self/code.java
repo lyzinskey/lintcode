49f1c8263a1a0c9f516e9f808465c61885a6b47e
Inskey <lyzinskey@gmail.com>
2018-12-29 17:27:16 -0800

    Create code.java

---
+++ 1310. Product of Array Except Self/code.java
@@ -0,0 +1,42 @@
+//Given an array of n integers where n > 1, nums, 
+//return an array output such that output[i] is equal to the product of all the elements of nums except nums[i].
+
+//Example
+//For example, given [1,2,3,4], return [24,12,8,6].
+
+//Challenge
+//Could you solve it with constant space complexity? 
+//(Note: The output array does not count as extra space for the purpose of space complexity analysis.)
+
+//Notice
+//Solve it without division and in O(n).
+
+
+
+
+public class Solution {
+    /**
+     * @param nums: an array of integers
+     * @return: the product of all the elements of nums except nums[i].
+     */
+    public int[] productExceptSelf(int[] nums) {   
+        if (nums.length == 1) {
+            return nums;
+        }
+        
+        int[] result = new int[nums.length];
+        result[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            result[i] = result[i - 1] * nums[i - 1];
+        }
+        int right = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            result[i] *= right;
+            right *= nums[i];        
+        }
+        return result;
+    }    
+}
+
+
+

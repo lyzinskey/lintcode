0a07c1b3bd831f49fe55ef3da2694f5ce83be846
Inskey <lyzinskey@gmail.com>
2019-01-04 16:19:55 -0800

    Update code.java

--- 135. Combination Sum/code.java
+++ 135. Combination Sum/code.java
@@ -15,42 +15,41 @@
 
 
 
+
+class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> result = new ArrayList<>();
         
-        if (candidates == null || target <= 0) {
+        if (candidates.length == 0) {
             return result;
         }
-        ArrayList<Integer> subset = new ArrayList<>();
-        Arrays.sort(candidates);
-        dfs(candidates, target, 0, result, subset);
         
+        Arrays.sort(candidates);
+        
+        DFS(candidates, target, result, new ArrayList<Integer>(), 0);
         return result;
     }
     
-    private void dfs(int[] candidates, int target, int index, List<List<Integer>> result, ArrayList<Integer> subset) {
-        if (target == 0) {
-            result.add(new ArrayList<Integer>(subset));
+    private void DFS(int[] candidates, int remain, List<List<Integer>> result, ArrayList<Integer> list, int start) {
+        if (remain < 0) {
+            return;
+        }
+        if (remain == 0) {
+            result.add(new ArrayList<Integer>(list));
             return;
         }
         
-        for (int i = index; i < candidates.length; i++) {
-            if (candidates[i] > target) {
-                break;
-            }
-            
-            //去重，[1, 1, 2, 2，2]保证只使用重复元素的第一个
-            //即只用第一个1和第一个2，第二个1和第二第三个2跳过
-            if (i > index && candidates[i] == candidates[i - 1]) {
+        for (int i = start; i < candidates.length && candidates[i] <= remain; i++) {
+            if (i > start && candidates[i] == candidates[i - 1]) {
                 continue;
             }
             
-            subset.add(candidates[i]);
-            //index传i而不是i + 1是因为一个元素可以使用多次
-            //注意这里与上方for循环去重的区别
-            dfs(candidates, target - candidates[i], i, result, subset);
-            subset.remove(subset.size() - 1);
+            list.add(candidates[i]);
+            DFS(candidates, remain - candidates[i], result, list, i);
+            list.remove(list.size() - 1);
         }
     }
-    
-    
+}
+
+
+

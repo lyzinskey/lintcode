a283fa0eb01ba0860abdd58b65169a81c494821f
Inskey <lyzinskey@gmail.com>
2019-11-26 15:13:23 -0800

    Update code.java

--- 110. Minimum Path Sum/code.java
+++ 110. Minimum Path Sum/code.java
@@ -28,24 +28,27 @@ public class Solution {
      * @return: An integer, minimizes the sum of all numbers along its path
      */
     public int minPathSum(int[][] grid) {
-        int[][] dp = new int[grid.length][grid[0].length];
+        int row = grid.length;
+        int col = grid[0].length;        
+        int[][] dp = new int[row][col];
+        dp[0][0] = grid[0][0];
         
-        for (int i = grid.length - 1; i >= 0; i--) {
-            for (int j = grid[0].length - 1; j >= 0; j--) {
-                if (i == grid.length - 1 && j == grid[0].length - 1) {
-                    dp[i][j] = grid[i][j];
-                } else if (i == grid.length - 1 && j != grid[0].length - 1) {
-                    dp[i][j] = grid[i][j] + dp[i][j + 1];
-                } else if (i != grid.length - 1 && j == grid[0].length - 1) {
-                    dp[i][j] = grid[i][j] + dp[i + 1][j];
-                } else {
-                    dp[i][j] = grid[i][j] + Math.min(dp[i + 1][j], dp[i][j + 1]);
-                }
+        for (int i = 1; i < row; i++) {
+            dp[i][0] = dp[i - 1][0] + grid[i][0];
+        }
+        for (int i = 1; i < col; i++) {
+            dp[0][i] = dp[0][i - 1] + grid[0][i];
+        }
+        
+        for (int i = 1; i < row; i++) {
+            for (int j = 1; j < col; j++) {
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
             }
         }
-        return dp[0][0];
+        return dp[row - 1][col - 1];
     }
 }
+
 
 
 

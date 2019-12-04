19189101789a53f62765a24d488659caa3c76a61
Inskey <lyzinskey@gmail.com>
2019-12-03 16:53:26 -0800

    Create code.java

---
+++ 1013. Unique Morse Code Words/code.java
@@ -0,0 +1,66 @@
+//International Morse Code defines a standard encoding where each letter is mapped to a series of dots and dashes, 
+//as follows: "a" maps to ".-", "b" maps to "-...", "c" maps to "-.-.", and so on.
+
+//For convenience, the full table for the 26 letters of the English alphabet is given below:
+
+//[".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+//Now, given a list of words, each word can be written as a concatenation of the Morse code of each letter. 
+//For example, "cab" can be written as "-.-.-....-", (which is the concatenation "-.-." + "-..." + ".-"). 
+//We'll call such a concatenation, the transformation of a word.
+
+//Return the number of different transformations among all words we have.
+
+//Example
+//Example 1:
+//
+//Input: words = ["gin", "zen", "gig", "msg"]
+//Output: 2
+//Explanation: 
+//The transformation of each word is:
+//"gin" -> "--...-."
+//"zen" -> "--...-."
+//"gig" -> "--...--."
+//"msg" -> "--...--."
+//
+//There are 2 different transformations, "--...-." and "--...--.".
+
+//Example 2:
+//
+//Input: words = ["a", "b"]
+//Output: 2
+//Explanation: 
+//The transformation of each word is:
+//"a" -> ".-"
+//"b" -> "-..."
+//There are 2 different transformations, ".-" and "-...".
+
+//Notice
+//The length of words will be at most 100.
+//Each words[i] will have length in range [1, 12].
+//words[i] will only consist of lowercase letters.
+
+
+
+
+public class Solution {
+    /**
+     * @param words: the given list of words
+     * @return: the number of different transformations among all words we have
+     */
+    public int uniqueMorseRepresentations(String[] words) {
+        String[] d = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."};
+        HashSet<String> s = new HashSet<>();
+        for (String w : words) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < w.length(); ++i) {
+                sb.append(d[w.charAt(i) - 'a']);
+            }                
+            s.add(sb.toString());
+        }
+        return s.size();
+    }    
+}
+
+
+
+

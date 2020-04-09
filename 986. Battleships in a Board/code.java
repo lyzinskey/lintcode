095da82ce7be8c9990e7a0d83cbabbb06a629658
Inskey <lyzinskey@gmail.com>
2020-04-08 20:29:02 -0700

    Create code.java

---
+++ 986. Battleships in a Board/code.java
@@ -0,0 +1,72 @@
+//Given an 2D board, count how many battleships are in it. 
+//The battleships are represented with 'X's, empty slots are represented with '.'s. 
+//You may assume the following rules:
+
+//You receive a valid board, made of only battleships or empty slots.
+//Battleships can only be placed horizontally or vertically. 
+//In other words, they can only be made of the shape 1xN (1 row, N columns) or Nx1 (N rows, 1 column), 
+//where N can be of any size.
+//At least one horizontal or vertical cell separates between two battleships - there are no adjacent battleships.
+
+//Example
+//Example1
+//
+//Input:
+//X..X
+//...X
+//...X
+//Output: 2
+//Explanation:
+//In the above board there are 2 battleships.
+
+//Example2
+//
+//Input:
+//...X
+//XXXX
+/...X
+//Explanation:
+//This is an invalid board that you will not receive - as battleships will always have a cell separating between them.
+
+//Challenge
+//Could you do it in one-pass, using only O(1) extra memory and without modifying the value of the board?
+
+
+
+
+public class Solution {
+    /**
+     * @param board: the given 2D board
+     * @return: the number of battle ships
+     */
+    // Time: O(mn)
+    // Space: O(1)     
+    public int countBattleships(char[][] board) {
+        if (board.length == 0 || board[0].length == 0) {
+            return 0;
+        }
+        
+        int row = board.length;
+        int col = board[0].length;
+        int count = 0;
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (board[i][j] == '.') {
+                    continue;
+                }
+                if (i > 0 && board[i - 1][j] == 'X') {
+                    continue;
+                }
+                if (j > 0 && board[i][j - 1] == 'X') {
+                    continue;
+                }
+                count++;
+            }
+        }
+        return count;
+    }
+}
+
+
+
